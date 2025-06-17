bf86d0b0a1be84c9f17572e20e534486ffad5613
sagun basnet <basnetsagun8@gmail.com>
2025-06-17 12:53:50 +0545

    js

---
+++ javascript/index.js
@@ -0,0 +1,44 @@
+// var b = false;
+
+// console.log(!a);
+
+// if (a || b) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+// while (a != 5) {
+//   console.log(a);
+//   a = a - 1;
+// }
+
+// do {
+//   console.log(a);
+//   a = a - 1;
+// } while (a != 3);
+
+// for (var i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+var a = 1;
+var b = 9;
+switch (a) {
+  case 1:
+    var c = a + b;
+    console.log("Add: " + c);
+    break;
+  case 2:
+    console.log("Value is 2");
+    break;
+  case 3:
+    console.log("Value is 3");
+    break;
+  case 4:
+    console.log("Value is 4");
+    break;
+  case 5:
+    console.log("Value is 5");
+    break;
+}
---
+++ javascript/main.js
@@ -0,0 +1,26 @@
+var a = 5;
+var b = 6;
+
+var d = a;
+
+var c = 7;
+
+console.log(!true);
+console.log("The sum of a and b is: " + c);
+
+// if (a === c) {
+//   console.log("a is equal to c");
+// } else {
+//   console.log("a is not equal to c");
+// }
+
+if (a % 2 === 0) {
+  console.log("a is even");
+}
+else if(num1 === num2){
+  console.log("Num1 is equal to Num2");
+  
+} 
+else {
+  console.log("a is odd");
+}
