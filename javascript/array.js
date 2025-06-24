34934d161cc00d50aac072472c0025159c838ad7
sagun basnet <basnetsagun8@gmail.com>
2025-06-24 12:30:12 +0545

    js dom

--- javascript/array.js
+++ javascript/array.js
@@ -1,37 +1,50 @@
-var arr = [
-  4,
-  8,
-  5,
-  "asdfasdf",
-  false,
-  12.43,
-  [1, 2, 3, 4, 5, 5],
-  { name: "jhon", age: 12, arr: [1, 2, 3, 5, 6] },
-];
+// var arr = [
+//   4,
+//   8,
+//   5,
+//   "asdfasdf",
+//   false,
+//   12.43,
+//   [1, 2, 3, 4, 5, 5],
+//   { name: "jhon", age: 12, arr: [1, 2, 3, 5, 6] },
+// ];
 
-console.log(arr[7].name);
+// console.log(arr[7].name);
 
-// for (var i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
+// // for (var i = 0; i < arr.length; i++) {
+// //   console.log(arr[i]);
+// // }
 
-var obj = {
-  name: "jhon",
-  age: 35,
-  address: {
-    country: "Nepal",
-    state: "Koshi",
-    city: "Itahari",
-  },
-  isNepali: true,
-  salary: 133000.0,
-  myArr: [23, 245, "asdf"],
-};
+// var obj = {
+//   name: "jhon",
+//   age: 35,
+//   address: {
+//     country: "Nepal",
+//     state: "Koshi",
+//     city: "Itahari",
+//   },
+//   isNepali: true,
+//   salary: 133000.0,
+//   myArr: [23, 245, "asdf"],
+// };
 
-console.log(obj.myArr[1]);
+// console.log(obj.myArr[1]);
 
 var myArr = [3, 12, 5, 8, 14, 8, 1];
+console.log("Before: ", myArr);
 
-function greatest(arr) {}
+var a = 10;
+// myArr.push(a);
+myArr.unshift(a);
 
-greatest(myArr);
+console.log("After: ", myArr);
+
+// console.log(myArr.pop());
+myArr.shift();
+myArr.shift();
+console.log("After shift: ", myArr);
+// console.log("After pop: ", myArr);
+
+// function greatest(arr) {}
+
+// greatest(myArr);
---
+++ javascript/dom.js
@@ -0,0 +1,12 @@
+function handleClik() {
+  console.log("Button Clicked");
+}
+
+// var btnnn = document.getElementsByClassName("btn");
+// var btnnn = document.getElementById("myButton");
+// var btnnn = document.getElementsByTagName("button");
+var btnnn = document.querySelector(".btn");
+var btnnns = document.querySelectorAll(".btn");
+
+console.log(btnnn);
+console.log(btnnns);
