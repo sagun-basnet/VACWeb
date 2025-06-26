88685f93a551f1ba2a2e266fdb6aacab7767f832
sagun basnet <basnetsagun8@gmail.com>
2025-06-26 11:59:10 +0545

    dom-2

--- javascript/dom1.js
+++ javascript/dom1.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector("button");
 const box = document.querySelector(".box");
 const reset = document.querySelector(".reset");
-
+const body = document.querySelector("body");
 btn.addEventListener("click", () => {
   box.style.backgroundColor = "black";
 });
