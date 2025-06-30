bc4516b886545b164a2c353d05c8575c3e1d21ff
sagun basnet <basnetsagun8@gmail.com>
2025-06-30 14:18:35 +0545

    innerHTML

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -13,3 +13,12 @@ cross.addEventListener("click", () => {
   bar.style.display = "block";
   cross.style.display = "none";
 });
+
+const heading = document.querySelector("#heading");
+const text = document.querySelector(".text");
+
+console.log(heading.innerHTML);
+heading.innerHTML = "Welcome to the Sidebar Example Example";
+
+text.innerHTML =
+  "This is a simple sidebar example using JavaScript. Click the bar icon to open the sidebar and the cross icon to close it.";
